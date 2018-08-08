4d5084da3bca07087bde1fddfc0d7991c76f8abc
Prashanth Menon <pmenon@cs.cmu.edu>
2018-08-07 21:32:05 -0400

    Cleanup CRC hash. Optimize by aligning buffer to byte boundary before processing.

--- src/include/util/hash.h
+++ src/include/util/hash.h
@@ -8,15 +8,15 @@ class Hasher {
  public:
   enum class HashMethod { Fnv1, Murmur3, Crc };
 
-  static uint32_t Hash(const char *data, uint64_t len,
+  static uint32_t Hash(const char *buf, uint64_t len,
                        HashMethod method = HashMethod::Crc);
 
  private:
-  static uint32_t HashFnv1(const char *data, uint64_t len);
+  static uint32_t HashFnv1(const char *buf, uint64_t len);
 
-  static uint32_t HashMurmur(const char *data, uint64_t len);
+  static uint32_t HashMurmur(const char *buf, uint64_t len);
 
-  static uint32_t HashCrc(const char *data, uint64_t len);
+  static uint32_t HashCrc(const char *buf, uint64_t len);
 };
 
 }  // namespace tpl::util
--- src/util/hash.cpp
+++ src/util/hash.cpp
@@ -4,29 +4,29 @@
 
 namespace tpl::util {
 
-uint32_t Hasher::Hash(const char *data, uint64_t len, HashMethod method) {
+uint32_t Hasher::Hash(const char *buf, uint64_t len, HashMethod method) {
   switch (method) {
     case HashMethod::Fnv1:
-      return HashFnv1(data, len);
+      return HashFnv1(buf, len);
     case HashMethod::Murmur3:
-      return HashMurmur(data, len);
+      return HashMurmur(buf, len);
     default:
-      return HashCrc(data, len);
+      return HashCrc(buf, len);
   }
 }
 
-uint32_t Hasher::HashFnv1(const char *data, uint64_t len) {
+uint32_t Hasher::HashFnv1(const char *buf, uint64_t len) {
   uint32_t hash = 2166136261u;
 
   for (uint64_t i = 0; i < len; i++) {
-    hash ^= data[i];
+    hash ^= buf[i];
     hash *= 16777619;
   }
 
   return hash;
 }
 
-uint32_t Hasher::HashMurmur(const char *data, uint64_t len) { return 0; }
+uint32_t Hasher::HashMurmur(const char *buf, uint64_t len) { return 0; }
 
 #define CRC32(op, crc, type, buf, len)                   \
   do {                                                   \
@@ -36,24 +36,27 @@ uint32_t Hasher::HashMurmur(const char *data, uint64_t len) { return 0; }
     }                                                    \
   } while (0)
 
-uint32_t Hasher::HashCrc(const char *data, uint64_t len) {
-  uint32_t crc = 0x741B8CD7;
+uint32_t Hasher::HashCrc(const char *buf, uint64_t len) {
+  static constexpr const auto kAlignSize = 0x08ul;
+  static constexpr const auto kAlignMask = (kAlignSize - 1);
+  static constexpr const uint32_t kSeed = 0x741b8cd7;
 
-  // If the string is empty, return the CRC calculated so far
-  if (len == 0) {
-    return crc;
+  uint32_t crc = kSeed;
+
+  // Align the input to the word boundary
+  for (; (len > 0) && ((size_t)buf & kAlignMask); len--, buf++) {
+    crc = _mm_crc32_u8(crc, *(uint8_t *)buf);
   }
 
+  // Now greedily process 8-byte, 4-byte, and 2-byte chunks of the input,
+  // exhausting the tail byte-at-a-time
 #if defined(__x86_64__) || defined(_M_X64)
-  // Try to each up as many 8-byte values as possible
-  CRC32(_mm_crc32_u64, crc, uint64_t, data, len);
+  CRC32(_mm_crc32_u64, crc, uint64_t, buf, len);
 #endif
-  // Now we perform CRC in 4-byte, then 2-byte chunks.  Finally, we process
-  // what remains in byte chunks.
-  CRC32(_mm_crc32_u32, crc, uint32_t, data, len);
-  CRC32(_mm_crc32_u16, crc, uint16_t, data, len);
-  CRC32(_mm_crc32_u8, crc, uint8_t, data, len);
-  // Done
+  CRC32(_mm_crc32_u32, crc, uint32_t, buf, len);
+  CRC32(_mm_crc32_u16, crc, uint16_t, buf, len);
+  CRC32(_mm_crc32_u8, crc, uint8_t, buf, len);
+
   return crc;
 }
 
