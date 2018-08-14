6cd41a994dc817f664f0e448154e35f679dc2e62
Prashanth Menon <pmenon@cs.cmu.edu>
2018-08-14 17:05:28 -0400

    Travis and deps file

---
+++ deps.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+## =================================================================
+## TPL PACKAGE INSTALLATION
+##
+## This script will install all the packages that are needed to
+## build and run the DBMS.
+##
+## Note: On newer versions of Ubuntu (17.04), this script
+## will not install the correct version of g++. You will have
+## to use 'update-alternatives' to configure the default of
+## g++ manually.
+##
+## Supported environments:
+##  * Ubuntu 18.04
+##  * MacOS
+## =================================================================
+
+main() {
+  set -o errexit
+
+    echo "PACKAGES WILL BE INSTALLED. THIS MAY BREAK YOUR EXISTING TOOLCHAIN."
+    echo "YOU ACCEPT ALL RESPONSIBILITY BY PROCEEDING."
+    read -p "Proceed? [Y/n] : " yn
+    case $yn in
+        Y|y) install;;
+        *) ;;
+    esac
+
+    echo "Script complete."
+}
+
+install() {
+  set -x
+  UNAME=$(uname | tr "[:lower:]" "[:upper:]" )
+
+  case $UNAME in
+    DARWIN) install_mac ;;
+
+    LINUX)
+      version=$(cat /etc/os-release | grep VERSION_ID | cut -d '"' -f 2)
+      case $version in
+        18.04) install_linux ;;
+        *) give_up ;;
+      esac
+      ;;
+
+    *) give_up ;;
+  esac
+}
+
+give_up() {
+  set +x
+  echo "Unsupported distribution '$UNAME'"
+  echo "Please contact our support team for additional help."
+  echo "Be sure to include the contents of this message."
+  echo "Platform: $(uname -a)"
+  echo
+  echo "https://github.com/pmenon/tpl/issues"
+  echo
+  exit 1
+}
+
+install_mac() {
+  # Install Homebrew.
+  if test ! $(which brew); then
+    echo "Installing Homebrew (https://brew.sh/)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+  # Update Homebrew.
+  brew update
+  # Install packages.
+  brew ls --versions cmake || brew install cmake
+  brew ls --versions git || brew install git
+  (brew ls --versions llvm | grep 6) || brew install llvm@6
+}
+
+install_linux() {
+  # Update apt-get.
+  apt-get -y update
+  # Install packages.
+  apt-get -y install \
+      build-essential \
+      clang-6.0 \
+      clang-format-6.0 \
+      cmake \
+      git \
+      g++-7 \
+      libjemalloc-dev \
+      libjsoncpp-dev \
+      libz-dev \
+      llvm-6.0
+}
+
+main "$@"
