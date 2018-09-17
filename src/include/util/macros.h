ae14afec64be050aa69ac25e427324b1f03e8b92
Prashanth Menon <pmenon@cs.cmu.edu>
2018-09-16 20:48:01 -0400

    Added force-inline macro

--- src/include/util/macros.h
+++ src/include/util/macros.h
@@ -6,6 +6,7 @@
 #include "llvm/Support/ErrorHandling.h"
 
 #define UNUSED __attribute__((unused))
+#define ALWAYS_INLINE __attribute__((noinline))
 
 #define DISALLOW_COPY_AND_MOVE(class)  \
   class(class &&) = delete;            \
@@ -13,8 +14,8 @@
   class &operator=(class &&) = delete; \
   class &operator=(const class &) = delete;
 
-#define TPL_LIKELY(x) __builtin_expect(!!(x), 1)
-#define TPL_UNLIKELY(x) __builtin_expect(!!(x), 0)
+#define TPL_LIKELY(x) LLVM_LIKELY(x)
+#define TPL_UNLIKELY(x) LLVM_UNLIKELY(x)
 
 #ifdef NDEBUG
 #define TPL_ASSERT(expr, msg) ((void)0)
