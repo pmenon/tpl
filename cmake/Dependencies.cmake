b1d13cbb6cc54ae38435e2a10a7547117c909afa
Prashanth Menon <pmenon@cs.cmu.edu>
2018-12-08 19:33:33 -0500

    Add dependencies

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -6,7 +6,7 @@ message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 if (${LLVM_PACKAGE_VERSION} VERSION_LESS "7")
     message(FATAL_ERROR "LLVM 7 or newer is required.")
 endif ()
-llvm_map_components_to_libnames(LLVM_LIBRARIES core mcjit nativecodegen native)
+llvm_map_components_to_libnames(LLVM_LIBRARIES core mcjit nativecodegen native ipo)
 include_directories(SYSTEM ${LLVM_INCLUDE_DIRS})
 list(APPEND TPL_LINK_LIBS ${LLVM_LIBRARIES})
 
