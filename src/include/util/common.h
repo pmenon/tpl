d1afdec8127ee3d69a616c9c9024656317387c84
Prashanth Menon <pmenon@cs.cmu.edu>
2019-01-31 18:24:39 -0500

    Fix includes

--- src/include/util/common.h
+++ src/include/util/common.h
@@ -13,6 +13,7 @@ using u8 = uint8_t;
 using u16 = uint16_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
+using u128 = unsigned __int128;
 using f32 = float;
 using f64 = double;
 using byte = std::byte;
--- src/include/util/vector_util.h
+++ src/include/util/vector_util.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <functional>
+
 #include "util/common.h"
 #include "util/simd.h"
 
