4d365877f0a9a5e25f85e06c50b01fab88a9e6a9
Prashanth Menon <pmenon@cs.cmu.edu>
2019-02-28 17:26:21 -0500

    More info

--- src/include/util/cpu_info.h
+++ src/include/util/cpu_info.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "util/bit_util.h"
 #include "util/common.h"
 #include "util/macros.h"
 
@@ -7,26 +8,46 @@ namespace tpl::util {
 
 class CpuInfo {
  public:
+  // -------------------------------------------------------
+  // CPU Features
+  // -------------------------------------------------------
+
+  enum Feature : u8 {
+    SSE_4_2 = 0,
+    AVX = 1,
+    AVX2 = 2,
+    AVX512 = 3,
+  };
+
+  // -------------------------------------------------------
+  // Caches
+  // -------------------------------------------------------
+
   // Cache levels
   enum CacheLevel : u8 { L1_CACHE = 0, L2_CACHE = 1, L3_CACHE = 2 };
-
-  // Number of caches
+  // Number of cache levels
   static constexpr const u32 kNumCacheLevels = CacheLevel::L3_CACHE + 1;
 
   /// Initialize
   static void Init();
 
+  /// Return the number of logical cores in the system
+  static u32 GetNumCores() { return kNumCores_; }
+
   /// Return the size of the cache at level \a level in bytes
-  static u64 GetCacheSize(CacheLevel level) {
-    TPL_ASSERT(initialized_, "CpuInfo not initialized! Call CpuInfo::Init()");
+  static u32 GetCacheSize(CacheLevel level) {
+    TPL_ASSERT(kInitialized_, "CpuInfo not initialized! Call CpuInfo::Init()");
     return kCacheSizes_[level];
   }
 
   /// Return the size of a cache line at level \a level
-  static u64 GetCacheLineSize(CacheLevel level) {
-    TPL_ASSERT(initialized_, "CpuInfo not initialized! Call CpuInfo::Init()");
+  static u32 GetCacheLineSize(CacheLevel level) {
+    TPL_ASSERT(kInitialized_, "CpuInfo not initialized! Call CpuInfo::Init()");
     return kCacheLineSizes_[level];
   }
+
+  /// Pretty print CPU information to a string
+  static std::string PrettyPrintInfo();
 
  private:
   // Dispatched from Init() to initialize various components
@@ -34,9 +55,13 @@ class CpuInfo {
   static void InitCacheInfo();
 
  private:
-  static bool initialized_;
-  static u64 kCacheSizes_[kNumCacheLevels];
-  static u64 kCacheLineSizes_[kNumCacheLevels];
+  static bool kInitialized_;
+  static u32 kNumCores_;
+  static std::string kModelName_;
+  static double kCpuMhz_;
+  static u32 kCacheSizes_[kNumCacheLevels];
+  static u32 kCacheLineSizes_[kNumCacheLevels];
+  static InlinedBitVector<64> kHardwareFlags_;
 };
 
 }  // namespace tpl::util
--- src/util/cpu_info.cpp
+++ src/util/cpu_info.cpp
@@ -1,32 +1,82 @@
 #include "util/cpu_info.h"
 
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringRef.h"
+
 #include <unistd.h>
+#include <fstream>
+#include <sstream>
 
 namespace tpl::util {
 
-bool CpuInfo::initialized_ = false;
-u64 CpuInfo::kCacheSizes_[CpuInfo::kNumCacheLevels];
-u64 CpuInfo::kCacheLineSizes_[CpuInfo::kNumCacheLevels];
+bool CpuInfo::kInitialized_ = false;
+u32 CpuInfo::kNumCores_ = 0;
+std::string CpuInfo::kModelName_;
+double CpuInfo::kCpuMhz_ = 0;
+u32 CpuInfo::kCacheSizes_[CpuInfo::kNumCacheLevels];
+u32 CpuInfo::kCacheLineSizes_[CpuInfo::kNumCacheLevels];
+InlinedBitVector<64> CpuInfo::kHardwareFlags_;
+
+struct {
+  CpuInfo::Feature feature;
+  llvm::SmallVector<const char *, 4> names;
+} interested_flags[] = {
+    {CpuInfo::SSE_4_2, {"sse4_2"}},
+    {CpuInfo::AVX, {"avx"}},
+    {CpuInfo::AVX2, {"avx2"}},
+    {CpuInfo::AVX512, {"avx512f", "avx512cd"}},
+};
+
+void ParseCpuFlags(UNUSED llvm::StringRef flags) {}
 
 void CpuInfo::InitCpuInfo() {
-  TPL_ASSERT(!initialized_, "Should not call post-initialization");
+  TPL_ASSERT(!kInitialized_, "Should not call post-initialization");
+
+#ifdef __APPLE__f
+#error "Fix me"
+#else
+  // On linux, just read /proc/cpuinfo
+  std::string line;
+  std::ifstream infile("/proc/cpuinfo");
+  while (std::getline(infile, line)) {
+    llvm::StringRef str(line);
+
+    auto [name, value] = str.split(":");
+    value = value.trim(" ");
+
+    if (name.startswith("processor")) {
+      kNumCores_++;
+    } else if (name.startswith("model")) {
+      kModelName_ = value.str();
+    } else if (name.startswith("cpu MHz")) {
+      double cpu_mhz;
+      value.getAsDouble(cpu_mhz);
+      kCpuMhz_ = std::max(kCpuMhz_, cpu_mhz);
+    } else if (name.startswith("flags")) {
+      ParseCpuFlags(value);
+    }
+  }
+#endif
 }
 
 void CpuInfo::InitCacheInfo() {
-  TPL_ASSERT(!initialized_, "Should not call post-initialization");
+  TPL_ASSERT(!kInitialized_, "Should not call post-initialization");
 
   // Use sysconf to determine cache sizes
-  kCacheSizes_[L1_CACHE] = static_cast<u64>(sysconf(_SC_LEVEL1_DCACHE_SIZE));
-  kCacheSizes_[L2_CACHE] = static_cast<u64>(sysconf(_SC_LEVEL2_CACHE_SIZE));
-  kCacheSizes_[L3_CACHE] = static_cast<u64>(sysconf(_SC_LEVEL3_CACHE_SIZE));
+  kCacheSizes_[L1_CACHE] = static_cast<u32>(sysconf(_SC_LEVEL1_DCACHE_SIZE));
+  kCacheSizes_[L2_CACHE] = static_cast<u32>(sysconf(_SC_LEVEL2_CACHE_SIZE));
+  kCacheSizes_[L3_CACHE] = static_cast<u32>(sysconf(_SC_LEVEL3_CACHE_SIZE));
 
-  kCacheLineSizes_[L1_CACHE] = static_cast<u64>(sysconf(_SC_LEVEL1_DCACHE_LINESIZE));
-  kCacheLineSizes_[L2_CACHE] = static_cast<u64>(sysconf(_SC_LEVEL2_CACHE_LINESIZE));
-  kCacheLineSizes_[L3_CACHE] = static_cast<u64>(sysconf(_SC_LEVEL3_CACHE_LINESIZE));
+  kCacheLineSizes_[L1_CACHE] =
+      static_cast<u32>(sysconf(_SC_LEVEL1_DCACHE_LINESIZE));
+  kCacheLineSizes_[L2_CACHE] =
+      static_cast<u32>(sysconf(_SC_LEVEL2_CACHE_LINESIZE));
+  kCacheLineSizes_[L3_CACHE] =
+      static_cast<u32>(sysconf(_SC_LEVEL3_CACHE_LINESIZE));
 }
 
 void CpuInfo::Init() {
-  if (initialized_) {
+  if (kInitialized_) {
     return;
   }
 
@@ -35,7 +85,24 @@ void CpuInfo::Init() {
   InitCacheInfo();
 
   // Mark done
-  initialized_ = true;
+  kInitialized_ = true;
+}
+
+std::string CpuInfo::PrettyPrintInfo() {
+  std::stringstream ss;
+
+  // clang-format off
+  ss << "CPU Info:" << std::endl;
+  ss << "  Model: " << kModelName_ << std::endl;
+  ss << "  Cores: " << kNumCores_ << std::endl;
+  ss << "  Mhz:   " << kCpuMhz_ << std::endl;
+  ss << "  Caches: " << kNumCacheLevels << std::endl;
+  ss << "    L1: " << (kCacheSizes_[L1_CACHE] / 1024.0) << " KB (" << kCacheLineSizes_[L1_CACHE] << " byte line)" << std::endl;
+  ss << "    L2: " << (kCacheSizes_[L2_CACHE] / 1024.0) << " KB (" << kCacheLineSizes_[L2_CACHE] << " byte line)" << std::endl;
+  ss << "    L3: " << (kCacheSizes_[L3_CACHE] / 1024.0) << " KB (" << kCacheLineSizes_[L3_CACHE] << " byte line)" << std::endl;
+  // clang-format on
+
+  return ss.str();
 }
 
 }  // namespace tpl::util
