f4ee00bf2e34b7f970f0fa46fc721cca61a93aba
Prashanth Menon <pmenon@cs.cmu.edu>
2019-03-06 13:28:46 -0500

    Moved stuff around

--- src/include/sql/sql.h
+++ src/include/sql/sql.h
@@ -4,6 +4,15 @@
 
 namespace tpl::sql {
 
+/// The possible column encodings
+enum class ColumnEncoding : u8 {
+  None,
+  Rle,
+  Delta,
+  IntegerDict,
+  StringDict,
+};
+
 /// All possible JOIN types
 enum class JoinType : u8 { Inner, Outer, Left, Right, Anti, Semi };
 
