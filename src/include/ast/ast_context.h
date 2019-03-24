80ab4bdb61dda470d8816fd6752c6016d16a608a
Wan Shen Lim <wanshenl@andrew.cmu.edu>
2019-03-23 21:28:34 -0400

    Format fixes.

--- src/include/ast/ast_context.h
+++ src/include/ast/ast_context.h
@@ -41,7 +41,7 @@ class AstContext {
 
   /// Is the function with name \a identifier a builtin function?
   /// \param[in] identifier The name of the function to check
-  /// \param[out] builtin If non-null, set to the appropriate builtin enumeration
+  /// \param[out] builtin Set to the appropriate builtin enumeration if non-null
   /// \return True if the function name is that of a builtin; false otherwise
   bool IsBuiltinFunction(Identifier identifier,
                          Builtin *builtin = nullptr) const;
