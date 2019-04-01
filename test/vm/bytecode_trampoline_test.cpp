0fa24969f2e499a1c3d6c228a41efea67add67e7
Prashanth Menon <pmenon@cs.cmu.edu>
2019-03-31 20:02:57 -0400

    More complex tests

--- test/vm/bytecode_trampoline_test.cpp
+++ test/vm/bytecode_trampoline_test.cpp
@@ -139,30 +139,88 @@ TEST_F(BytecodeTrampolineTest, BigIntFunctionTest) {
   }
 }
 
-TEST_F(BytecodeTrampolineTest, GeneratedSorterTest) {
-  // Generate a few random numbers
-  const u32 nelems = 10;
-  std::vector<i32> numbers(nelems);
-  std::random_device random;
-  std::generate(numbers.begin(), numbers.end(),
-                [&random]() { return random() % 100; });
+TEST_F(BytecodeTrampolineTest, CodeGenComparisonFunctionSorterTest) {
+  //
+  // Test 1: Sort a list of signed 32-bit signed integers using a generated TPL
+  //         function. The list contains integers in the range [0, 100] and
+  //         will be sorted in ascending order.
+  //
 
-  // Generate the comparison function that sorts ascending
-  BytecodeCompiler compiler;
-  auto src = "fun compare(a: *int32, b: *int32) -> bool { return *a < *b }";
-  auto module = compiler.CompileToModule(src);
-  EXPECT_FALSE(compiler.HasErrors());
-  auto compare = reinterpret_cast<bool (*)(const i32 *, const i32 *)>(
-      module->GetFuncTrampoline(module->GetFuncInfoByName("compare")->id()));
-  EXPECT_TRUE(compare != nullptr);
+  {
+    const u32 nelems = 100;
+    std::vector<i32> numbers(nelems);
+    std::random_device random;
+    std::generate(numbers.begin(), numbers.end(),
+                  [&random]() { return random() % 100; });
 
-  // Try to sort using the generated comparison function
-  std::sort(
-      numbers.begin(), numbers.end(),
-      [compare](const auto &a, const auto &b) { return compare(&a, &b); });
+    // Generate the comparison function that sorts ascending
+    auto src = "fun compare(a: *int32, b: *int32) -> bool { return *a < *b }";
 
-  // Verify
-  EXPECT_TRUE(std::is_sorted(numbers.begin(), numbers.end()));
+    // Compile
+    BytecodeCompiler compiler;
+    auto module = compiler.CompileToModule(src);
+    EXPECT_FALSE(compiler.HasErrors());
+    auto compare = reinterpret_cast<bool (*)(const i32 *, const i32 *)>(
+        module->GetFuncTrampoline(module->GetFuncInfoByName("compare")->id()));
+    EXPECT_TRUE(compare != nullptr);
+
+    // Try to sort using the generated comparison function
+    std::sort(
+        numbers.begin(), numbers.end(),
+        [compare](const auto &a, const auto &b) { return compare(&a, &b); });
+
+    // Verify
+    EXPECT_TRUE(std::is_sorted(numbers.begin(), numbers.end()));
+  }
+
+  //
+  // Test 2: Sort a list of custom structures using a custom generated TPL
+  //         function. Each struct is composed of four 32-bit integers, a, b, c,
+  //         and d. All integers are in the range [0, 100]. The list is sorted
+  //         ascending by the 'c' field.
+  //
+
+  {
+    struct S {
+      i32 a, b, c, d;
+      S(i32 a, i32 b, i32 c, i32 d) : a(a), b(b), c(c), d(d) {}
+    };
+
+    const u32 nelems = 100;
+    std::vector<S> elems;
+    std::random_device random;
+    for (u32 i = 0; i < nelems; i++) {
+      elems.emplace_back(random() % 5, random() % 10, random() % 100,
+                         random() % 1000);
+    };
+
+    // Generate the comparison function that sorts ascending by S.c
+    auto src = R"(
+    struct S {
+      a: int32
+      b: int32
+      c: int32
+      d: int32
+    }
+    fun compare(a: *S, b: *S) -> bool { return a.c < b.c })";
+
+    BytecodeCompiler compiler;
+    auto module = compiler.CompileToModule(src);
+    EXPECT_FALSE(compiler.HasErrors());
+    auto compare = reinterpret_cast<bool (*)(const S *, const S *)>(
+        module->GetFuncTrampoline(module->GetFuncInfoByName("compare")->id()));
+    EXPECT_TRUE(compare != nullptr);
+
+    // Try to sort using the generated comparison function
+    std::sort(
+        elems.begin(), elems.end(),
+        [compare](const auto &a, const auto &b) { return compare(&a, &b); });
+
+    // Verify
+    EXPECT_TRUE(
+        std::is_sorted(elems.begin(), elems.end(),
+                       [](const auto &a, const auto &b) { return a.c < b.c; }));
+  }
 }
 
 }  // namespace tpl::vm::test
