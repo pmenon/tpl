32f54d709136df6565d8f47176cf415d1637410b
Prashanth Menon <pmenon@cs.cmu.edu>
2019-04-28 14:01:58 -0400

    Remove 'uint64_t' usage from hashing.
    Tune: The Weeknd - Wicked Games

--- src/util/hash.cpp
+++ src/util/hash.cpp
@@ -6,7 +6,7 @@
 
 namespace tpl::util {
 
-hash_t Hasher::Hash(const u8 *buf, uint64_t len, HashMethod method) {
+hash_t Hasher::Hash(const u8 *buf, u64 len, HashMethod method) {
   switch (method) {
     case HashMethod::Fnv1:
       return HashFnv1(buf, len);
@@ -17,7 +17,7 @@ hash_t Hasher::Hash(const u8 *buf, uint64_t len, HashMethod method) {
   }
 }
 
-hash_t Hasher::HashFnv1(const u8 *buf, uint64_t len) {
+hash_t Hasher::HashFnv1(const u8 *buf, u64 len) {
   auto hash = hash_t(2166136261ull);
 
   for (u64 i = 0; i < len; i++) {
@@ -28,14 +28,14 @@ hash_t Hasher::HashFnv1(const u8 *buf, uint64_t len) {
   return hash;
 }
 
-hash_t Hasher::HashMurmur3(const u8 *buf, uint64_t len) { return 0; }
+hash_t Hasher::HashMurmur3(const u8 *buf, u64 len) { return 0; }
 
-hash_t Hasher::HashCrc32(const u8 *buf, uint64_t len) {
+hash_t Hasher::HashCrc32(const u8 *buf, u64 len) {
   // Thanks HyPer
   auto gen_hash_64 = [](u64 input, u64 seed) {
     u64 result1 = _mm_crc32_u64(seed, input);
     u64 result2 = _mm_crc32_u64(0x04C11DB7, input);
-    return ((result2 << 32) | result1) * 0x2545F4914F6CDD1Dull;
+    return ((result2 << 32u) | result1) * 0x2545F4914F6CDD1Dull;
   };
 
   u64 hash = 0;
@@ -55,10 +55,10 @@ hash_t Hasher::HashCrc32(const u8 *buf, uint64_t len) {
   // Process the tail
   switch (len) {
     case 3:
-      hash ^= (static_cast<u64>(buf[2])) << 16;
+      hash ^= (static_cast<u64>(buf[2])) << 16u;
       FALLTHROUGH;
     case 2:
-      hash ^= (static_cast<u64>(buf[1])) << 8;
+      hash ^= (static_cast<u64>(buf[1])) << 8u;
       FALLTHROUGH;
     case 1:
       hash ^= buf[0];
