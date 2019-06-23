accc0aafc07f9733818767b43910d5d80248e165
Prashanth Menon <pmenon@cs.cmu.edu>
2019-06-23 09:53:52 -0400

    Memory pool pointers

--- src/include/sql/memory_pool.h
+++ src/include/sql/memory_pool.h
@@ -14,6 +14,108 @@ namespace tpl::sql {
 class MemoryTracker;
 
 /**
+ * A thin wrapper around a pointer to an object allocated from a memory pool.
+ * Exists to alert users that this object has to be returned to the pool at
+ * some point. Also to ensure users don't try to delete objects allocated from
+ * pool (if they were returned as raw pointers).
+ * @tparam T The type of the pointed-to object.
+ */
+template <typename T>
+class MemPoolPtr {
+ public:
+  /**
+   * Null pointer.
+   */
+  MemPoolPtr() : obj_(nullptr) {}
+  explicit MemPoolPtr(nullptr_t) : obj_(nullptr) {}
+
+  /**
+   * Copy.
+   */
+  MemPoolPtr(const MemPoolPtr<T> &ptr) : obj_(ptr.obj_) {}
+
+  /**
+   * Move.
+   */
+  MemPoolPtr(MemPoolPtr<T> &&ptr) noexcept : obj_(ptr.obj_) {
+    ptr.obj_ = nullptr;
+  }
+
+  /**
+   * Arrow operation.
+   */
+  T *operator->() { return obj_; }
+
+  /**
+   * Arrow operation.
+   */
+  const T *operator->() const { return obj_; }
+
+  /**
+   * Move assignment to force users to move objects rather than copy.
+   */
+  MemPoolPtr<T> &operator=(MemPoolPtr<T> &&that) noexcept {
+    obj_ = that.obj_;
+    that.obj_ = nullptr;
+    return *this;
+  }
+
+  /**
+   * Raw access.
+   */
+  T *get() { return obj_; }
+
+  /**
+   * Raw access.
+   */
+  const T *get() const { return obj_; }
+
+  /**
+   * Does this pointer point to anything?
+   */
+  explicit operator bool() const { return obj_; }
+
+ private:
+  friend class MemoryPool;
+
+  // Creating from raw pointers only allowed by MemoryPool
+  explicit MemPoolPtr(T *obj) : obj_(obj) {}
+
+ private:
+  T *obj_;
+};
+
+template <typename T1, typename T2>
+inline bool operator==(const MemPoolPtr<T1> &a, const MemPoolPtr<T2> &b) {
+  return a.get() == b.get();
+}
+
+template <typename T>
+inline bool operator==(std::nullptr_t, const MemPoolPtr<T> &b) noexcept {
+  return !b;
+}
+
+template <typename T>
+inline bool operator==(const MemPoolPtr<T> &a, std::nullptr_t) noexcept {
+  return !a;
+}
+
+template <typename T1, typename T2>
+inline bool operator!=(const MemPoolPtr<T1> &a, const MemPoolPtr<T2> &b) {
+  return a.get() != b.get();
+}
+
+template <typename T>
+inline bool operator!=(std::nullptr_t, const MemPoolPtr<T> &a) noexcept {
+  return (bool)a;
+}
+
+template <typename T>
+inline bool operator!=(const MemPoolPtr<T> &a, std::nullptr_t) noexcept {
+  return (bool)a;
+}
+
+/**
  * A memory pool
  */
 class MemoryPool {
@@ -78,6 +180,20 @@ class MemoryPool {
   }
 
   /**
+   * Allocate an object from from this pool.
+   * @tparam T The type of the object to allocate.
+   * @tparam Args The argument types.
+   * @param args The arguments.
+   * @return A construct object allocated from this pool.
+   */
+  template <typename T, typename... Args>
+  MemPoolPtr<T> NewObject(Args &&... args) {
+    auto *object_mem = Allocate(sizeof(T), true);
+    auto *obj = new (object_mem) T(std::forward<Args>(args)...);
+    return MemPoolPtr<T>(obj);
+  }
+
+  /**
    * Deallocate memory allocated by this pool.
    * @param ptr The pointer to the memory.
    * @param size The size of the allocation.
@@ -94,6 +210,17 @@ class MemoryPool {
   template <typename T>
   void DeallocateArray(T *const ptr, const std::size_t num_elems) {
     Deallocate(ptr, sizeof(T) * num_elems);
+  }
+
+  /**
+   * Free an object allocated from this pool.
+   * @tparam T The type of the object.
+   * @param object The object.
+   */
+  template <typename T>
+  void FreeObject(MemPoolPtr<T> &&object) {
+    object->~T();
+    Deallocate(object.get(), sizeof(T));
   }
 
   /**
---
+++ test/sql/memory_pool_test.cpp
@@ -0,0 +1,60 @@
+#include "tpl_test.h"  // NOLINT
+
+#include "sql/memory_pool.h"
+
+namespace tpl::sql::test {
+
+class MemoryPoolTest : public TplTest {};
+
+struct SimpleObj {
+  u32 a, b, c, d;
+};
+
+struct ComplexObj {
+  MemoryPool *memory;
+  MemPoolPtr<SimpleObj> nested;
+  ComplexObj(MemoryPool *m, MemPoolPtr<SimpleObj> n)
+      : memory(m), nested(std::move(n)) {}
+  ~ComplexObj() { memory->FreeObject(std::move(nested)); }
+};
+
+TEST_F(MemoryPoolTest, PoolPointers) {
+  MemoryPool pool(nullptr);
+
+  // Empty pointer
+  MemPoolPtr<SimpleObj> obj1, obj2;
+  EXPECT_EQ(obj1, obj2);
+  EXPECT_EQ(nullptr, obj1);
+  EXPECT_EQ(obj1, nullptr);
+  EXPECT_EQ(nullptr, obj1.get());
+
+  // Non-empty pointers
+  obj1 = pool.NewObject<SimpleObj>();
+  EXPECT_NE(obj1, obj2);
+  EXPECT_NE(nullptr, obj1);
+  EXPECT_NE(obj1, nullptr);
+  EXPECT_NE(nullptr, obj1.get());
+
+  obj1->a = 10;
+  EXPECT_EQ(10u, obj1->a);
+
+  pool.FreeObject(std::move(obj1));
+}
+
+TEST_F(MemoryPoolTest, ComplexPointers) {
+  MemoryPool pool(nullptr);
+
+  MemPoolPtr<ComplexObj> obj1, obj2;
+  EXPECT_EQ(obj1, obj2);
+
+  obj1 = pool.NewObject<ComplexObj>(&pool, pool.NewObject<SimpleObj>());
+  EXPECT_NE(nullptr, obj1);
+  EXPECT_NE(obj1, nullptr);
+
+  obj1->nested->a = 1;
+  EXPECT_EQ(1u, obj1->nested->a);
+
+  pool.FreeObject(std::move(obj1));
+}
+
+}  // namespace tpl::sql::test
