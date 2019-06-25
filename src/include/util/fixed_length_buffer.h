9faa3abfbfed326c00b88fd396c803d1721bfdc9
Prashanth Menon <pmenon@cs.cmu.edu>
2019-06-25 10:13:42 -0400

    Lint

--- src/include/util/fixed_length_buffer.h
+++ src/include/util/fixed_length_buffer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iterator>
+
 namespace tpl::util {
 
 /**
@@ -16,6 +18,21 @@ namespace tpl::util {
 template <typename T, u32 N>
 class FixedLengthBuffer {
  public:
+  using value_type = T;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using iterator = value_type *;
+  using const_iterator = const value_type *;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+  /**
+   * Create an empty buffer.
+   */
   FixedLengthBuffer() : index_(0) {}
 
   /**
@@ -25,7 +42,7 @@ class FixedLengthBuffer {
    * otherwise.
    * @param val The value to append to the buffer.
    */
-  void append(const T val) { buf_[index_++] = val; }
+  void append(const value_type &val) { buf_[index_++] = val; }
 
   /**
    * Clear elements from the buffer, resetting the size to zero.
@@ -35,7 +52,12 @@ class FixedLengthBuffer {
   /**
    * Return the number of elements currently in the buffer.
    */
-  u32 size() const noexcept { return index_; }
+  size_type size() const noexcept { return index_; }
+
+  /**
+   * Maximum capacity of this buffer.
+   */
+  size_type capacity() const noexcept { return N; }
 
   /**
    * Is this buffer empty?
@@ -49,7 +71,7 @@ class FixedLengthBuffer {
    * @param idx The index of the element to access.
    * @return A const reference to the element in this buffer at a given index.
    */
-  const T &operator[](const u32 idx) const noexcept {
+  constexpr const_reference operator[](const u32 idx) const noexcept {
     TPL_ASSERT(idx < size(), "Out-of-bounds buffer access");
     return buf_[idx];
   }
@@ -61,7 +83,7 @@ class FixedLengthBuffer {
    * @param idx The index of the element to access.
    * @return A const reference to the element in this buffer at a given index.
    */
-  T &operator[](const u32 idx) noexcept {
+  constexpr reference operator[](const u32 idx) noexcept {
     TPL_ASSERT(idx < size(), "Out-of-bounds buffer access");
     return buf_[idx];
   }
@@ -71,7 +93,7 @@ class FixedLengthBuffer {
    * @param idx The index of the element to access.
    * @return A const reference to the element in this buffer at a given index.
    */
-  const T &at(const u32 idx) const {
+  constexpr const_reference at(const u32 idx) const {
     if (idx >= size()) {
       throw std::out_of_range("Out-of-range access");
     }
@@ -83,7 +105,7 @@ class FixedLengthBuffer {
    * @param idx The index of the element to access.
    * @return A reference to the element in this buffer at a given index.
    */
-  T &at(const u32 idx) {
+  constexpr reference at(const u32 idx) {
     if (idx >= size()) {
       throw std::out_of_range("Out-of-range access");
     }
@@ -94,10 +116,14 @@ class FixedLengthBuffer {
   // Iterators
   // -------------------------------------------------------
 
-  T *begin() { return buf_.begin(); }
-  T *end() { return begin() + size(); }
-  const T *begin() const { return buf_.begin(); }
-  const T *end() const { return begin() + size(); }
+  constexpr iterator begin() { return buf_.begin(); }
+  constexpr iterator end() { return begin() + size(); }
+  constexpr const_iterator begin() const { return buf_.begin(); }
+  constexpr const_iterator end() const { return begin() + size(); }
+  constexpr reverse_iterator rbegin() { return reverse_iterator(end()); }
+  constexpr reverse_iterator rend() { return reverse_iterator(begin()); }
+  constexpr reverse_iterator crbegin() { return const_reverse_iterator(end()); }
+  constexpr reverse_iterator crend() { return const_reverse_iterator(begin()); }
 
  private:
   // The backing container.
