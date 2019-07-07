0d9c973a8782e2f68428804d8c7219b96ad11a9e
Prashanth Menon <pmenon@cs.cmu.edu>
2019-07-07 05:17:38 -0400

    Tests

--- test/sql/vector_operations/vector_opertions_test.cpp
+++ test/sql/vector_operations/vector_opertions_test.cpp
@@ -2,6 +2,7 @@
 
 #include "tpl_test.h"  // NOLINT
 
+#include "sql/constant_vector.h"
 #include "sql/vector.h"
 #include "sql/vector_operations/vector_operators.h"
 
@@ -54,11 +55,173 @@ TEST_F(VectorOperationsTest, Fill) {
   CHECK_SIMPLE_FILL(Double, f64(-3.14));
 }
 
-TEST_F(VectorOperationsTest, CompareEqual) {
-  const u32 num_elems = 100;
+TEST_F(VectorOperationsTest, CompareWithConstant) {
+  // Input vector: [0,1,2,3,4,5]
+  Vector vec(TypeId::BigInt, true, false);
+  vec.set_count(6);
+  VectorOps::Generate(&vec, 0, 1);
+
+  for (auto type_id : {TypeId::TinyInt, TypeId::SmallInt, TypeId::Integer,
+                       TypeId::BigInt, TypeId::Float, TypeId::Double}) {
+    vec.Cast(type_id);
+
+    // Try to find
+    ConstantVector _4(GenericValue::CreateBigInt(4).CastTo(type_id));
+    Vector result(TypeId::Boolean, true, true);
+
+    // Only index 4 == 4
+    {
+      auto check = [&]() {
+        EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(0));
+        EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(1));
+        EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(2));
+        EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(3));
+        EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(4));
+        EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(5));
+      };
+      VectorOps::Equal(vec, _4, &result);
+      check();
+      VectorOps::Equal(_4, vec, &result);
+      check();
+    }
+
+    // Only index 5 > 4
+    {
+      VectorOps::GreaterThan(vec, _4, &result);
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(0));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(1));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(2));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(3));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(4));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(5));
+    }
+
+    // Indexes 4-5 are >= 4
+    {
+      VectorOps::GreaterThanEqual(vec, _4, &result);
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(0));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(1));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(2));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(3));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(4));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(5));
+    }
+
+    // Indexes 0-3 are < 4
+    {
+      VectorOps::LessThan(vec, _4, &result);
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(0));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(1));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(2));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(3));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(4));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(5));
+    }
+
+    // Indexes 0-4 are < 4
+    {
+      VectorOps::LessThanEqual(vec, _4, &result);
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(0));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(1));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(2));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(3));
+      EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(4));
+      EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(5));
+    }
+
+    // Indexes 0,1,2,3,5 are != 4
+    {
+      auto check = [&]() {
+        EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(0));
+        EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(1));
+        EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(2));
+        EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(3));
+        EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(4));
+        EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(5));
+      };
+      VectorOps::NotEqual(vec, _4, &result);
+      check();
+      VectorOps::NotEqual(_4, vec, &result);
+      check();
+    }
+  }
+}
+
+TEST_F(VectorOperationsTest, CompareWithNulls) {
+  const u32 num_elems = 6;
   Vector vec(TypeId::BigInt, true, false);
   vec.set_count(num_elems);
   VectorOps::Generate(&vec, 0, 1);
+
+  ConstantVector _null(GenericValue::CreateNull(TypeId::BigInt));
+  Vector result(TypeId::Boolean, true, true);
+
+  VectorOps::Equal(vec, _null, &result);
+  EXPECT_TRUE(result.GetValue(0).is_null());
+  EXPECT_TRUE(result.GetValue(1).is_null());
+  EXPECT_TRUE(result.GetValue(2).is_null());
+  EXPECT_TRUE(result.GetValue(3).is_null());
+  EXPECT_TRUE(result.GetValue(4).is_null());
+  EXPECT_TRUE(result.GetValue(5).is_null());
+}
+
+TEST_F(VectorOperationsTest, NullChecking) {
+  Vector vec(TypeId::Float, true, false);
+  vec.set_count(4);
+
+  vec.SetValue(0, GenericValue::CreateFloat(1.0));
+  vec.SetValue(1, GenericValue::CreateNull(TypeId::Float));
+  vec.SetValue(2, GenericValue::CreateFloat(1.0));
+  vec.SetValue(3, GenericValue::CreateNull(TypeId::Float));
+
+  {
+    Vector result(TypeId::Boolean, true, true);
+    VectorOps::IsNull(vec, &result);
+    EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(0));
+    EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(1));
+    EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(2));
+    EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(3));
+  }
+
+  {
+    Vector result(TypeId::Boolean, true, true);
+    VectorOps::IsNotNull(vec, &result);
+    EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(0));
+    EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(1));
+    EXPECT_EQ(GenericValue::CreateBoolean(true), result.GetValue(2));
+    EXPECT_EQ(GenericValue::CreateBoolean(false), result.GetValue(3));
+  }
+}
+
+TEST_F(VectorOperationsTest, AnyOrAllTrue) {
+  Vector vec(TypeId::Boolean, true, false);
+  vec.set_count(4);
+
+  vec.SetValue(0, GenericValue::CreateBoolean(false));
+  vec.SetValue(1, GenericValue::CreateBoolean(false));
+  vec.SetValue(2, GenericValue::CreateBoolean(false));
+  vec.SetValue(3, GenericValue::CreateBoolean(false));
+
+  EXPECT_FALSE(VectorOps::AnyTrue(vec));
+  EXPECT_FALSE(VectorOps::AllTrue(vec));
+
+  vec.SetValue(3, GenericValue::CreateNull(TypeId::Boolean));
+
+  EXPECT_FALSE(VectorOps::AnyTrue(vec));
+  EXPECT_FALSE(VectorOps::AllTrue(vec));
+
+  vec.SetValue(3, GenericValue::CreateBoolean(true));
+
+  EXPECT_TRUE(VectorOps::AnyTrue(vec));
+  EXPECT_FALSE(VectorOps::AllTrue(vec));
+
+  vec.SetValue(0, GenericValue::CreateBoolean(true));
+  vec.SetValue(1, GenericValue::CreateBoolean(true));
+  vec.SetValue(2, GenericValue::CreateBoolean(true));
+  vec.SetValue(3, GenericValue::CreateBoolean(true));
+
+  EXPECT_TRUE(VectorOps::AnyTrue(vec));
+  EXPECT_TRUE(VectorOps::AllTrue(vec));
 }
 
 }  // namespace tpl::sql::test
