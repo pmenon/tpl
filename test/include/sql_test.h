2c34ec98819b0eb1a73eed848832a717b87a34c2
Prashanth Menon <pmenon@cs.cmu.edu>
2019-07-11 10:24:16 -0400

    Test APIs for vectors

--- test/include/sql_test.h
+++ test/include/sql_test.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <memory>
+
 #include "gtest/gtest.h"
 
 #include "tpl_test.h"  // NOLINT
 
 #include "sql/catalog.h"
+#include "sql/vector.h"
 
 namespace tpl {
 
@@ -19,5 +22,35 @@ class SqlBasedTest : public TplTest {
     return static_cast<u16>(table_id);
   }
 };
+
+#define MAKE_VEC_TYPE_IMPL(NAME, TYPE, CPP_TYPE)                             \
+  static inline std::unique_ptr<sql::Vector> Make##NAME##Vector(             \
+      const std::vector<CPP_TYPE> &vals, const std::vector<bool> &nulls) {   \
+    auto vec = std::make_unique<sql::Vector>(sql::TypeId::TYPE, true, true); \
+    vec->set_count(vals.size());                                             \
+    for (u32 i = 0; i < vals.size(); i++) {                                  \
+      if (nulls[i]) {                                                        \
+        vec->SetValue(i, sql::GenericValue::CreateNull(vec->type_id()));     \
+      } else {                                                               \
+        vec->SetValue(i, sql::GenericValue::Create##NAME(vals[i]));          \
+      }                                                                      \
+    }                                                                        \
+    return vec;                                                              \
+  }
+
+#define MAKE_VEC_TYPE(TYPE, CPP_TYPE) MAKE_VEC_TYPE_IMPL(TYPE, TYPE, CPP_TYPE)
+
+MAKE_VEC_TYPE(Boolean, bool)
+MAKE_VEC_TYPE(TinyInt, i8)
+MAKE_VEC_TYPE(SmallInt, i16)
+MAKE_VEC_TYPE(Integer, i32)
+MAKE_VEC_TYPE(BigInt, i64)
+MAKE_VEC_TYPE(Float, f32)
+MAKE_VEC_TYPE(Double, f64)
+MAKE_VEC_TYPE_IMPL(String, Varchar, const char *)
+MAKE_VEC_TYPE_IMPL(String, Varchar, std::string)
+
+#undef MAKE_VEC_TYPE
+#undef MAKE_VEC_TYPE_IMPL
 
 }  // namespace tpl
