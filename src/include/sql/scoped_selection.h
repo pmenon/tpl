5965fc8114ee630aa2e58e76973befa91c372460
Prashanth Menon <pmenon@cs.cmu.edu>
2019-08-11 06:09:23 -0700

    Agnostic to container types

--- src/include/sql/scoped_selection.h
+++ src/include/sql/scoped_selection.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <vector>
-
 #include "sql/vector.h"
 
 namespace tpl::sql {
@@ -10,33 +8,43 @@ namespace tpl::sql {
  * Scope class to temporarily and atomically switch selected/visible tuples for
  * a collection of input vectors. The original selection vector is switched back
  * into all input vectors when this class goes out of scope and is destroyed.
+ *
+ * @tparam ContainerT The type of container storing all vector instances. Must
+ *                    provide forward iteration.
  */
+template <typename ContainerT>
 class SelectionScope {
+  static_assert(
+      std::is_base_of_v<
+          std::forward_iterator_tag,
+          typename std::iterator_traits<
+              typename ContainerT::const_iterator>::iterator_category>,
+      "Container provided to SelectionScope must provide support for forward "
+      "iteration");
+
  public:
-  SelectionScope(sel_t *sel_vector, u32 count,
-                 const std::vector<Vector *> &vectors)
+  SelectionScope(sel_t *sel_vector, const u32 count, const ContainerT &vectors)
       : vectors_(vectors) {
     old_sel_vector_ = vectors_[0]->selection_vector();
     old_count_ = vectors_[0]->count();
-    for (auto *vector : vectors_) {
-#ifndef NDEBUG
-      TPL_ASSERT(vector->selection_vector() == old_sel_vector_ &&
-                     vector->count() == old_count_,
-                 "All vectors must have the same selection vector and count");
-#endif
+    for (Vector *vector : vectors_) {
+      TPL_ASSERT(vector->selection_vector() == old_sel_vector_,
+                 "All vectors must have the same selection vector");
+      TPL_ASSERT(vector->count() == old_count_,
+                 "All vectors must have the same count");
       vector->SetSelectionVector(sel_vector, count);
     }
   }
 
   ~SelectionScope() {
-    for (auto *vector : vectors_) {
+    for (Vector *vector : vectors_) {
       vector->SetSelectionVector(old_sel_vector_, old_count_);
     }
   }
 
  private:
   // The list of vectors we're controlling
-  const std::vector<Vector *> &vectors_;
+  const ContainerT &vectors_;
   // The previous selection vector
   sel_t *old_sel_vector_;
   // The previous count of the selection vector, or the original size of the
