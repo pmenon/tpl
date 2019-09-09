396a28578ffac1b86e289709d5fd26a12349694b
Prashanth Menon <pmenon@cs.cmu.edu>
2019-09-09 16:40:33 -0400

    Cleanup

--- src/sql/tuple_id_list.cpp
+++ src/sql/tuple_id_list.cpp
@@ -8,8 +8,8 @@
 namespace tpl::sql {
 
 uint32_t TupleIdList::AsSelectionVector(uint16_t *sel_vec) const {
-  return util::VectorUtil::BitVectorToSelectionVector(bit_vector_.data_array(),
-                                                      bit_vector_.num_bits(), sel_vec);
+  return util::VectorUtil::BitVectorToSelectionVector(bit_vector_.words(), bit_vector_.num_bits(),
+                                                      sel_vec);
 }
 
 std::string TupleIdList::ToString() const {
