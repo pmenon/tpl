fb8769bacc5811f9f7e908ad89cd913df7fccdb3
Prashanth Menon <pmenon@cs.cmu.edu>
2019-09-12 21:44:04 -0400

    Exceptions

---
+++ src/include/common/exception.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include <cstdarg>
+#include <stdexcept>
+
+#include "spdlog/fmt/fmt.h"
+
+#include "common/macros.h"
+#include "sql/sql.h"
+
+namespace tpl {
+
+/**
+ * All exception types.
+ */
+enum class ExceptionType {
+  OutOfRange,      // value out of range error
+  Conversion,      // conversion/casting error
+  UnknownType,     // unknown type
+  TypeMismatch,    // mismatched types
+  Decimal,         // decimal related
+  DivideByZero,    // divide by 0
+  InvalidType,     // incompatible for operation
+  NotImplemented,  // method not implemented
+  Execution,       // executor related
+  Index,           // index related
+};
+
+/**
+ * Base exception class.
+ */
+class Exception : public std::exception {
+ public:
+  /**
+   * Construct an exception with the given type and message.
+   * @param exception_type The type of the exception.
+   * @param message The message to print.
+   */
+  Exception(ExceptionType exception_type, const std::string &message);
+
+  /**
+   * Return the error message.
+   * @return
+   */
+  const char *what() const noexcept override;
+
+  /**
+   * Convert an exception type into its string representation.
+   * @param type The type of exception.
+   * @return The string name of the input exception type.
+   */
+  static std::string ExceptionTypeToString(ExceptionType type);
+
+ protected:
+  template <typename... Args>
+  void Format(const Args &... args) {
+    exception_message_ = fmt::format(exception_message_, args...);
+  }
+
+ private:
+  // The type of the exception
+  ExceptionType type_;
+
+  // The message
+  std::string exception_message_;
+};
+
+std::ostream &operator<<(std::ostream &os, const Exception &e);
+
+// ---------------------------------------------------------
+// Concrete exceptions
+// ---------------------------------------------------------
+
+/**
+ * An exception thrown due to an invalid cast.
+ */
+class CastException : public Exception {
+ public:
+  CastException(sql::TypeId src_type, sql::TypeId dest_type)
+      : Exception(ExceptionType::Conversion, "Type {} cannot be cast as {}") {
+    Format(TypeIdToString(src_type), TypeIdToString(dest_type));
+  }
+};
+
+/**
+ * An exception thrown when a value falls outside a given type's valid value range.
+ */
+class ValueOutOfRangeException : public Exception {
+ public:
+  template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>, uint32_t>>
+  ValueOutOfRangeException(T value, sql::TypeId src_type, sql::TypeId dest_type)
+      : ValueOutOfRangeException() {
+    Format(TypeIdToString(src_type), TypeIdToString(dest_type));
+  }
+
+ private:
+  ValueOutOfRangeException()
+      : Exception(ExceptionType::OutOfRange,
+                  "Type {} with value {} cannot be cast because the value is out of range for the "
+                  "target type {}") {}
+};
+
+/**
+ * An exception thrown when a given type cannot be converted into another type.
+ */
+class ConversionException : public Exception {
+ public:
+  template <typename... Args>
+  explicit ConversionException(const std::string &msg, const Args &... args)
+      : Exception(ExceptionType::Conversion, msg) {
+    Format(args...);
+  }
+};
+
+class TypeMismatchException : public Exception {
+ public:
+  TypeMismatchException(sql::TypeId src_type, sql::TypeId dest_type, const std::string &msg)
+      : Exception(ExceptionType::TypeMismatch, "Type '{}' does not match type '{}'. " + msg) {
+    Format(TypeIdToString(src_type), TypeIdToString(dest_type));
+  }
+};
+
+/**
+ *
+ */
+class InvalidTypeException : public Exception {
+ public:
+  InvalidTypeException(sql::TypeId type, const std::string &msg)
+      : Exception(ExceptionType::InvalidType, "Invalid type ['{}']: " + msg) {
+    Format(TypeIdToString(type));
+  }
+};
+
+/**
+ * An exception thrown to indicate some functionality isn't implemented.
+ */
+class NotImplementedException : public Exception {
+ public:
+  template <typename... Args>
+  explicit NotImplementedException(const std::string &msg, const Args &... args)
+      : Exception(ExceptionType::NotImplemented, msg) {
+    Format(args...);
+  }
+};
+
+}  // namespace tpl
