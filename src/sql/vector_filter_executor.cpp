4990b8caca75c4b9fd0d68c0fdc3e1ddf3beac9d
Prashanth Menon <pmenon@cs.cmu.edu>
2019-09-13 23:14:35 -0400

    Cleanup

--- src/sql/vector_filter_executor.cpp
+++ src/sql/vector_filter_executor.cpp
@@ -35,104 +35,104 @@ void VectorFilterExecutor::SelectGeneric(const std::vector<uint32_t> &col_indexe
 }
 
 void VectorFilterExecutor::SelectEqVal(const uint32_t col_idx, const GenericValue &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
   VectorOps::SelectEqual(*left_vector, ConstantVector(val), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectEqVal(uint32_t col_idx, const Val &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
-  auto constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
+  const GenericValue constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
   VectorOps::SelectEqual(*left_vector, ConstantVector(constant), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectGeVal(const uint32_t col_idx, const GenericValue &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
   VectorOps::SelectGreaterThanEqual(*left_vector, ConstantVector(val), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectGeVal(uint32_t col_idx, const Val &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
-  auto constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
+  const GenericValue constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
   VectorOps::SelectGreaterThanEqual(*left_vector, ConstantVector(constant), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectGtVal(const uint32_t col_idx, const GenericValue &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
   VectorOps::SelectGreaterThan(*left_vector, ConstantVector(val), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectGtVal(uint32_t col_idx, const Val &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
-  auto constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
+  const GenericValue constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
   VectorOps::SelectGreaterThan(*left_vector, ConstantVector(constant), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectLeVal(const uint32_t col_idx, const GenericValue &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
   VectorOps::SelectLessThanEqual(*left_vector, ConstantVector(val), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectLeVal(uint32_t col_idx, const Val &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
-  auto constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
+  const GenericValue constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
   VectorOps::SelectLessThanEqual(*left_vector, ConstantVector(constant), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectLtVal(const uint32_t col_idx, const GenericValue &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
   VectorOps::SelectLessThan(*left_vector, ConstantVector(val), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectLtVal(uint32_t col_idx, const Val &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
-  auto constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
+  const GenericValue constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
   VectorOps::SelectLessThan(*left_vector, ConstantVector(constant), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectNeVal(const uint32_t col_idx, const GenericValue &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
   VectorOps::SelectNotEqual(*left_vector, ConstantVector(val), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectNeVal(uint32_t col_idx, const Val &val) {
-  auto left_vector = vector_projection_->GetColumn(col_idx);
-  auto constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
+  const Vector *left_vector = vector_projection_->GetColumn(col_idx);
+  const GenericValue constant = GenericValue::CreateFromRuntimeValue(left_vector->type_id(), val);
   VectorOps::SelectNotEqual(*left_vector, ConstantVector(constant), &tid_list_);
 }
 
 void VectorFilterExecutor::SelectEq(const uint32_t left_col_idx, const uint32_t right_col_idx) {
-  auto left_vector = vector_projection_->GetColumn(left_col_idx);
-  auto right_vector = vector_projection_->GetColumn(right_col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(left_col_idx);
+  const Vector *right_vector = vector_projection_->GetColumn(right_col_idx);
   VectorOps::SelectEqual(*left_vector, *right_vector, &tid_list_);
 }
 
 void VectorFilterExecutor::SelectGe(const uint32_t left_col_idx, const uint32_t right_col_idx) {
-  auto left_vector = vector_projection_->GetColumn(left_col_idx);
-  auto right_vector = vector_projection_->GetColumn(right_col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(left_col_idx);
+  const Vector *right_vector = vector_projection_->GetColumn(right_col_idx);
   VectorOps::SelectGreaterThanEqual(*left_vector, *right_vector, &tid_list_);
 }
 
 void VectorFilterExecutor::SelectGt(const uint32_t left_col_idx, const uint32_t right_col_idx) {
-  auto left_vector = vector_projection_->GetColumn(left_col_idx);
-  auto right_vector = vector_projection_->GetColumn(right_col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(left_col_idx);
+  const Vector *right_vector = vector_projection_->GetColumn(right_col_idx);
   VectorOps::SelectGreaterThan(*left_vector, *right_vector, &tid_list_);
 }
 
 void VectorFilterExecutor::SelectLe(const uint32_t left_col_idx, const uint32_t right_col_idx) {
-  auto left_vector = vector_projection_->GetColumn(left_col_idx);
-  auto right_vector = vector_projection_->GetColumn(right_col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(left_col_idx);
+  const Vector *right_vector = vector_projection_->GetColumn(right_col_idx);
   VectorOps::SelectLessThanEqual(*left_vector, *right_vector, &tid_list_);
 }
 
 void VectorFilterExecutor::SelectLt(const uint32_t left_col_idx, const uint32_t right_col_idx) {
-  auto left_vector = vector_projection_->GetColumn(left_col_idx);
-  auto right_vector = vector_projection_->GetColumn(right_col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(left_col_idx);
+  const Vector *right_vector = vector_projection_->GetColumn(right_col_idx);
   VectorOps::SelectLessThan(*left_vector, *right_vector, &tid_list_);
 }
 
 void VectorFilterExecutor::SelectNe(const uint32_t left_col_idx, const uint32_t right_col_idx) {
-  auto left_vector = vector_projection_->GetColumn(left_col_idx);
-  auto right_vector = vector_projection_->GetColumn(right_col_idx);
+  const Vector *left_vector = vector_projection_->GetColumn(left_col_idx);
+  const Vector *right_vector = vector_projection_->GetColumn(right_col_idx);
   VectorOps::SelectNotEqual(*left_vector, *right_vector, &tid_list_);
 }
 
