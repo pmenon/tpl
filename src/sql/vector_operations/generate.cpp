cf5f6d13f41e8efa6bf445e6ff0f4d01abea7293
Prashanth Menon <pmenon@cs.cmu.edu>
2019-09-14 12:21:59 -0400

    Cleanup

--- src/sql/vector_operations/generate.cpp
+++ src/sql/vector_operations/generate.cpp
@@ -1,11 +1,13 @@
 #include "sql/vector_operations/vector_operators.h"
 
+#include "common/exception.h"
+
 namespace tpl::sql {
 
 namespace {
 
 template <typename T>
-void GenerateSequenceImpl(Vector *vector, T start, T increment) {
+void TemplatedGenerateOperation(Vector *vector, T start, T increment) {
   auto *data = reinterpret_cast<T *>(vector->data());
   auto value = start;
   VectorOps::Exec(*vector, [&](uint64_t i, uint64_t k) {
@@ -18,45 +20,44 @@ void GenerateSequenceImpl(Vector *vector, T start, T increment) {
 
 void VectorOps::Generate(tpl::sql::Vector *vector, int64_t start, int64_t increment) {
   if (!IsTypeNumeric(vector->type_id())) {
-    throw std::runtime_error("Sequence generation only allowed on numeric vectors");
+    throw InvalidTypeException(vector->type_id(),
+                               "sequence generation only allowed on numeric vectors");
   }
   switch (vector->type_id()) {
     case TypeId::TinyInt: {
-      GenerateSequenceImpl<int8_t>(vector, start, increment);
+      TemplatedGenerateOperation<int8_t>(vector, start, increment);
       break;
     }
     case TypeId::SmallInt: {
-      GenerateSequenceImpl<int16_t>(vector, start, increment);
+      TemplatedGenerateOperation<int16_t>(vector, start, increment);
       break;
     }
     case TypeId::Integer: {
-      GenerateSequenceImpl<int32_t>(vector, start, increment);
+      TemplatedGenerateOperation<int32_t>(vector, start, increment);
       break;
     }
     case TypeId::BigInt: {
-      GenerateSequenceImpl<int64_t>(vector, start, increment);
+      TemplatedGenerateOperation<int64_t>(vector, start, increment);
       break;
     }
     case TypeId::Hash: {
-      GenerateSequenceImpl<hash_t>(vector, start, increment);
+      TemplatedGenerateOperation<hash_t>(vector, start, increment);
       break;
     }
     case TypeId::Pointer: {
-      GenerateSequenceImpl<uintptr_t>(vector, start, increment);
+      TemplatedGenerateOperation<uintptr_t>(vector, start, increment);
       break;
     }
     case TypeId::Float: {
-      GenerateSequenceImpl<float>(vector, start, increment);
+      TemplatedGenerateOperation<float>(vector, start, increment);
       break;
     }
     case TypeId::Double: {
-      GenerateSequenceImpl<double>(vector, start, increment);
+      TemplatedGenerateOperation<double>(vector, start, increment);
       break;
     }
     default: {
-      UNREACHABLE(
-          "Impossible type in switch. Should have been caught in if-guard "
-          "earlier!");
+      UNREACHABLE("Impossible type in switch. Should have been caught in if-guard earlier!");
     }
   }
 }
