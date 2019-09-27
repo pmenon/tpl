a4198cb6ec1eacc8b62affd670a8655dc0b17690
Prashanth Menon <pmenon@cs.cmu.edu>
2019-09-27 14:14:11 -0400

    Renamed files. Fixed Q1 parallel.

--- src/include/sql/tablegen/table_generator.h
+++ src/include/sql/tablegen/table_generator.h
@@ -32,11 +32,17 @@ class TPCHOutputSchemas {
    * @return the schema if it exists; an exception otherwise
    */
   const Schema *GetSchema(const std::string &name) {
-    auto iter = schemas_.find(name);
-    return iter == schemas_.end() ? nullptr : iter->second.get();
+    auto iter = schemas_by_name_.find(name);
+    return iter == schemas_by_name_.end() ? nullptr : owned_schemas_[iter->second].get();
   }
 
  private:
+  void AddSchema(std::unique_ptr<Schema> schema, const std::string &table_name) {
+    owned_schemas_.emplace_back(std::move(schema));
+    schemas_by_name_.emplace(table_name, owned_schemas_.size() - 1);
+    schemas_by_name_.emplace(table_name + "_par", owned_schemas_.size() - 1);
+  }
+
   void InitTPCHOutput() {
     const auto &int_type = IntegerType::Instance(false);
     const auto &real_type = RealType::Instance(false);
@@ -46,69 +52,66 @@ class TPCHOutputSchemas {
     Schema::ColumnInfo real_col{"dummy", real_type};
     Schema::ColumnInfo date_col{"dummy", date_type};
     Schema::ColumnInfo string_col{"dummy", string_type};
+
     // Q1 (two strings, 7 reals, 1 int)
     {
-      std::vector<Schema::ColumnInfo> cols;
-      for (uint32_t i = 0; i < uint32_t(2); i++) {
-        cols.emplace_back(string_col);
-      }
-      for (uint32_t i = 0; i < uint32_t(7); i++) {
-        cols.emplace_back(real_col);
-      }
-      cols.emplace_back(int_col);
-      schemas_.emplace("tpch_q1", std::make_unique<Schema>(std::move(cols)));
+      std::vector<Schema::ColumnInfo> cols = {string_col, string_col, real_col, real_col, real_col,
+                                              real_col,   real_col,   real_col, real_col, int_col};
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q1");
     }
 
-    // Q6 (one real)
-    {
-      std::vector<Schema::ColumnInfo> cols{real_col};
-      schemas_.emplace("tpch_q6", std::make_unique<Schema>(std::move(cols)));
-    }
     // Q4 (one string, one int)
     {
       std::vector<Schema::ColumnInfo> cols{string_col, int_col};
-      schemas_.emplace("tpch_q4", std::make_unique<Schema>(std::move(cols)));
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q4");
     }
 
     // Q5 (one string, one real)
     {
       std::vector<Schema::ColumnInfo> cols{string_col, real_col};
-      schemas_.emplace("tpch_q5", std::make_unique<Schema>(std::move(cols)));
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q5");
+    }
+
+    // Q6 (one real)
+    {
+      std::vector<Schema::ColumnInfo> cols{real_col};
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q6");
     }
 
     // Q7 (two strings, one int, one real)
     {
       std::vector<Schema::ColumnInfo> cols{string_col, string_col, int_col, real_col};
-      schemas_.emplace("tpch_q7", std::make_unique<Schema>(std::move(cols)));
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q7");
     }
 
     // Q11 (one int, one real)
     {
       std::vector<Schema::ColumnInfo> cols{int_col, real_col};
-      schemas_.emplace("tpch_q11", std::make_unique<Schema>(std::move(cols)));
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q11");
     }
 
     // Q16 (two strings, two ints)
     {
       std::vector<Schema::ColumnInfo> cols{string_col, string_col, int_col, int_col};
-      schemas_.emplace("tpch_q16", std::make_unique<Schema>(std::move(cols)));
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q16");
     }
 
     // Q18 (one string, two integers, one date, two reals)
     {
       std::vector<Schema::ColumnInfo> cols{string_col, int_col,  int_col,
                                            date_col,   real_col, real_col};
-      schemas_.emplace("tpch_q18", std::make_unique<Schema>(std::move(cols)));
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q18");
     }
 
     // Q19 (one real)
     {
       std::vector<Schema::ColumnInfo> cols{real_col};
-      schemas_.emplace("tpch_q19", std::make_unique<Schema>(std::move(cols)));
+      AddSchema(std::make_unique<Schema>(std::move(cols)), "q19");
     }
   }
 
-  std::unordered_map<std::string, std::unique_ptr<Schema>> schemas_;
+  std::unordered_map<std::string, std::size_t> schemas_by_name_;
+  std::vector<std::unique_ptr<Schema>> owned_schemas_;
 };
 
 }  // namespace tpl::sql::tablegen
