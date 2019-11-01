7e0fdc77b2031b4a813802de55e4bcf450988f2d
Prashanth Menon <pmenon@cs.cmu.edu>
2019-11-01 16:34:38 -0400

    Cleanup

--- src/sql/vector_operations/null.cpp
+++ src/sql/vector_operations/null.cpp
@@ -5,14 +5,12 @@
 namespace tpl::sql {
 
 void VectorOps::IsNull(const Vector &input, TupleIdList *tid_list) {
-  TPL_ASSERT(input.GetSize() == tid_list->GetCapacity(),
-             "Input vector and TID list have mismatched sizes");
+  TPL_ASSERT(input.GetSize() == tid_list->GetCapacity(), "Input vector size != TID list size");
   tid_list->GetMutableBits()->Intersect(input.GetNullMask());
 }
 
 void VectorOps::IsNotNull(const Vector &input, TupleIdList *tid_list) {
-  TPL_ASSERT(input.GetSize() == tid_list->GetCapacity(),
-             "Input vector and TID list have mismatched sizes");
+  TPL_ASSERT(input.GetSize() == tid_list->GetCapacity(), "Input vector size != TID list size");
   tid_list->GetMutableBits()->Difference(input.GetNullMask());
 }
 
