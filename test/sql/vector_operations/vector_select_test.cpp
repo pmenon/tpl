74bcd8df1f8f15a9df091791c42ea49258776b69
Prashanth Menon <pmenon@cs.cmu.edu>
2019-11-22 23:56:05 -0500

    GCC 9

--- test/sql/vector_operations/vector_select_test.cpp
+++ test/sql/vector_operations/vector_select_test.cpp
@@ -171,10 +171,10 @@ TEST_F(VectorSelectTest, SelectNullConstant) {
 }
 
 TEST_F(VectorSelectTest, StringSelection) {
-  auto a = MakeVarcharVector({"His palm's are sweaty", nullptr, "arms are heavy",
+  auto a = MakeVarcharVector({"His palm's are sweaty", {}, "arms are heavy",
                               "vomit on his sweater already", "mom's spaghetti"},
                              {false, true, false, false, false});
-  auto b = MakeVarcharVector({"He's nervous", "but on the surface he looks calm and ready", nullptr,
+  auto b = MakeVarcharVector({"He's nervous", "but on the surface he looks calm and ready", {},
                               "to drop bombs", "but he keeps on forgetting"},
                              {false, false, true, false, false});
   auto tid_list = TupleIdList(a->GetSize());
