ebdf44330afd6aa8468617ba5b83ffafa395b91f
Prashanth Menon <pmenon@cs.cmu.edu>
2020-02-15 15:00:01 -0500

    Randoms

--- src/include/util/fast_rand.h
+++ src/include/util/fast_rand.h
@@ -7,16 +7,16 @@
 namespace tpl::util {
 
 /**
- * Fast-random number generater based on Lehmer's technique.
+ * Fast-random number generator based on Lehmer's technique.
  *
- * D. H. Lehmer, Mathematical methods in large-scale computing units.
- * Proceedings of a Second Symposium on Large Scale Digital Calculating
- * Machinery;
+ * D. H. Lehmer.
+ * Mathematical methods in large-scale computing units.
+ * Proceedings of a Second Symposium on Large Scale Digital Calculating Machinery;
  * Annals of the Computation Laboratory, Harvard Univ. 26 (1951), pp. 141-146.
  *
- * P L'Ecuyer,  Tables of linear congruential generators of different sizes and
- * good lattice structure. Mathematics of Computation of the American
- * Mathematical
+ * P L'Ecuyer.
+ * Tables of linear congruential generators of different sizes and good lattice structure.
+ * Mathematics of Computation of the American Mathematical
  * Society 68.225 (1999): 249-260.
  */
 class FastRand {
@@ -25,10 +25,9 @@ class FastRand {
    * Create a new fast random number generator.
    */
   FastRand() {
-    // Seed this fast random number generator with a number generated using a
-    // slower PRNG.
-    std::random_device rand_dev;
-    std::mt19937_64 mt(rand_dev());
+    // Seed using a number generated from a slow PRNG.
+    std::random_device rd;
+    std::mt19937_64 mt(rd());
     std::uniform_int_distribution<uint64_t> dist;
     state_ = dist(mt);
   }
