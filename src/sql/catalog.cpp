486ec21eddcc62895d07add52002f4f31cacccf5
Prashanth Menon <pmenon@cs.cmu.edu>
2020-02-18 11:43:18 -0500

    Fix catalog table initialization to use per-column counters for serial data.

--- src/sql/catalog.cpp
+++ src/sql/catalog.cpp
@@ -34,12 +34,13 @@ struct ColumnInsertMeta {
   const char *name;
   const SqlType &sql_type;
   Dist dist;
+  uint64_t serial_counter;
   std::variant<int64_t, double> min;
   std::variant<int64_t, double> max;
 
   ColumnInsertMeta(const char *name, const SqlType &sql_type, Dist dist,
                    std::variant<int64_t, double> min, std::variant<int64_t, double> max)
-      : name(name), sql_type(sql_type), dist(dist), min(min), max(max) {}
+      : name(name), sql_type(sql_type), dist(dist), serial_counter(0), min(min), max(max) {}
 };
 
 /**
@@ -111,12 +112,12 @@ auto GetRandomDistribution(T min, T max)
   return std::uniform_real_distribution<T>(min, max);
 }
 
-bool *CreateBooleanColumnData(Dist dist, uint32_t num_vals) {
+bool *CreateBooleanColumnData(ColumnInsertMeta *col_meta, uint32_t num_vals) {
   auto *val = static_cast<bool *>(Memory::MallocAligned(sizeof(bool) * num_vals, CACHELINE_SIZE));
 
-  switch (dist) {
+  switch (col_meta->dist) {
     case Dist::Uniform: {
-      std::mt19937 generator{};
+      std::mt19937 generator(std::random_device{}());
       std::uniform_int_distribution<int16_t> distribution(0, 1);
       for (uint32_t i = 0; i < num_vals; i++) {
         val[i] = distribution(generator) % 2 == 0;
@@ -140,13 +141,12 @@ bool *CreateBooleanColumnData(Dist dist, uint32_t num_vals) {
 }
 
 template <typename T>
-T *CreateNumberColumnData(Dist dist, uint32_t num_vals, T min, T max) {
-  static T serial_counter = 0;
+T *CreateNumberColumnData(ColumnInsertMeta *col_meta, uint32_t num_vals, T min, T max) {
   auto *val = static_cast<T *>(Memory::MallocAligned(sizeof(T) * num_vals, CACHELINE_SIZE));
 
-  switch (dist) {
+  switch (col_meta->dist) {
     case Dist::Uniform: {
-      auto generator = std::mt19937{};
+      std::mt19937 generator(std::random_device{}());
       auto distribution = GetRandomDistribution(min, max);
 
       for (uint32_t i = 0; i < num_vals; i++) {
@@ -156,8 +156,8 @@ T *CreateNumberColumnData(Dist dist, uint32_t num_vals, T min, T max) {
       break;
     }
     case Dist::Serial: {
-      for (uint32_t i = 0; i < num_vals; i++) {
-        val[i] = serial_counter++;
+      for (uint32_t i = 0; i < num_vals; i++, col_meta->serial_counter++) {
+        val[i] = static_cast<T>(col_meta->serial_counter);
       }
       break;
     }
@@ -169,61 +169,56 @@ T *CreateNumberColumnData(Dist dist, uint32_t num_vals, T min, T max) {
   return val;
 }
 
-std::pair<byte *, uint32_t *> GenerateColumnData(const ColumnInsertMeta &col_meta,
-                                                 uint32_t num_rows) {
+std::pair<byte *, uint32_t *> GenerateColumnData(ColumnInsertMeta *col_meta, uint32_t num_rows) {
   // Create data
   byte *col_data = nullptr;
-  switch (col_meta.sql_type.GetId()) {
+  switch (col_meta->sql_type.GetId()) {
     case SqlTypeId::Boolean: {
-      col_data = reinterpret_cast<byte *>(CreateBooleanColumnData(col_meta.dist, num_rows));
+      col_data = reinterpret_cast<byte *>(CreateBooleanColumnData(col_meta, num_rows));
       break;
     }
     case SqlTypeId::TinyInt: {
-      col_data = reinterpret_cast<byte *>(
-          CreateNumberColumnData<int8_t>(col_meta.dist, num_rows, std::get<int64_t>(col_meta.min),
-                                         std::get<int64_t>(col_meta.max)));
+      col_data = reinterpret_cast<byte *>(CreateNumberColumnData<int8_t>(
+          col_meta, num_rows, std::get<int64_t>(col_meta->min), std::get<int64_t>(col_meta->max)));
       break;
     }
     case SqlTypeId::SmallInt: {
-      col_data = reinterpret_cast<byte *>(
-          CreateNumberColumnData<int16_t>(col_meta.dist, num_rows, std::get<int64_t>(col_meta.min),
-                                          std::get<int64_t>(col_meta.max)));
+      col_data = reinterpret_cast<byte *>(CreateNumberColumnData<int16_t>(
+          col_meta, num_rows, std::get<int64_t>(col_meta->min), std::get<int64_t>(col_meta->max)));
       break;
     }
     case SqlTypeId::Integer: {
-      col_data = reinterpret_cast<byte *>(
-          CreateNumberColumnData<int32_t>(col_meta.dist, num_rows, std::get<int64_t>(col_meta.min),
-                                          std::get<int64_t>(col_meta.max)));
+      col_data = reinterpret_cast<byte *>(CreateNumberColumnData<int32_t>(
+          col_meta, num_rows, std::get<int64_t>(col_meta->min), std::get<int64_t>(col_meta->max)));
       break;
     }
     case SqlTypeId::BigInt:
     case SqlTypeId::Decimal: {
-      col_data = reinterpret_cast<byte *>(
-          CreateNumberColumnData<int64_t>(col_meta.dist, num_rows, std::get<int64_t>(col_meta.min),
-                                          std::get<int64_t>(col_meta.max)));
+      col_data = reinterpret_cast<byte *>(CreateNumberColumnData<int64_t>(
+          col_meta, num_rows, std::get<int64_t>(col_meta->min), std::get<int64_t>(col_meta->max)));
       break;
     }
     case SqlTypeId::Real: {
       col_data = reinterpret_cast<byte *>(CreateNumberColumnData<float>(
-          col_meta.dist, num_rows, std::get<double>(col_meta.min), std::get<double>(col_meta.max)));
+          col_meta, num_rows, std::get<double>(col_meta->min), std::get<double>(col_meta->max)));
       break;
     }
     case SqlTypeId::Double: {
       col_data = reinterpret_cast<byte *>(CreateNumberColumnData<double>(
-          col_meta.dist, num_rows, std::get<double>(col_meta.min), std::get<double>(col_meta.max)));
+          col_meta, num_rows, std::get<double>(col_meta->min), std::get<double>(col_meta->max)));
       break;
     }
     case SqlTypeId::Date:
     case SqlTypeId::Char:
     case SqlTypeId::Varchar: {
       throw NotImplementedException(
-          fmt::format("Populating column type '{}'", col_meta.sql_type.GetName()));
+          fmt::format("Populating column type '{}'", col_meta->sql_type.GetName()));
     }
   }
 
   // Create bitmap
   uint32_t *null_bitmap = nullptr;
-  if (col_meta.sql_type.IsNullable()) {
+  if (col_meta->sql_type.IsNullable()) {
     TPL_ASSERT(num_rows != 0, "Cannot have 0 rows.");
     uint64_t num_words = util::BitUtil::Num32BitWordsFor(num_rows);
     null_bitmap = static_cast<uint32_t *>(
@@ -233,21 +228,21 @@ std::pair<byte *, uint32_t *> GenerateColumnData(const ColumnInsertMeta &col_met
   return {col_data, null_bitmap};
 }
 
-void InitTable(const TableInsertMeta &table_meta, Table *table) {
-  LOG_INFO("Populating table instance '{}' with {} rows", table_meta.name, table_meta.num_rows);
+void InitTable(TableInsertMeta *table_meta, Table *table) {
+  LOG_INFO("Populating table instance '{}' with {} rows", table_meta->name, table_meta->num_rows);
 
   uint32_t batch_size = 10000;
-  uint32_t num_batches = table_meta.num_rows / batch_size +
-                         static_cast<uint32_t>(table_meta.num_rows % batch_size != 0);
+  uint32_t num_batches = table_meta->num_rows / batch_size +
+                         static_cast<uint32_t>(table_meta->num_rows % batch_size != 0);
 
   for (uint32_t i = 0; i < num_batches; i++) {
     std::vector<ColumnSegment> columns;
 
     // Generate column data for all columns
-    uint32_t num_vals = std::min(batch_size, table_meta.num_rows - (i * batch_size));
+    uint32_t num_vals = std::min(batch_size, table_meta->num_rows - (i * batch_size));
     TPL_ASSERT(num_vals != 0, "Can't have empty columns.");
-    for (const auto &col_meta : table_meta.col_meta) {
-      auto [data, null_bitmap] = GenerateColumnData(col_meta, num_vals);
+    for (auto &col_meta : table_meta->col_meta) {
+      auto [data, null_bitmap] = GenerateColumnData(&col_meta, num_vals);
       columns.emplace_back(col_meta.sql_type, data, null_bitmap, num_vals);
     }
 
@@ -281,8 +276,8 @@ Catalog::Catalog() : next_table_id_(static_cast<uint16_t>(TableId::Last)) {
   }
 
   // Populate all tables
-  for (const auto &table_meta : insert_meta) {
-    InitTable(table_meta, LookupTableById(static_cast<uint16_t>(table_meta.id)));
+  for (auto &table_meta : insert_meta) {
+    InitTable(&table_meta, LookupTableById(static_cast<uint16_t>(table_meta.id)));
   }
 
   LOG_INFO("Catalog initialization complete");
