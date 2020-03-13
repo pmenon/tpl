0d6174c2e70729802b6c67b75b3f91b58f9f4efe
Prashanth Menon <pmenon@cs.cmu.edu>
2020-03-13 10:41:54 -0700

    Fixes

--- src/sql/codegen/operators/projection_translator.cpp
+++ src/sql/codegen/operators/projection_translator.cpp
@@ -6,10 +6,20 @@
 
 namespace tpl::sql::codegen {
 
+// The majority of work for projections are performed during expression
+// evaluation. In the context of projections, expressions are derived when
+// requesting an output from the expression.
+
 ProjectionTranslator::ProjectionTranslator(const planner::ProjectionPlanNode &plan,
                                            CompilationContext *compilation_context,
                                            Pipeline *pipeline)
-    : OperatorTranslator(plan, compilation_context, pipeline) {}
+    : OperatorTranslator(plan, compilation_context, pipeline) {
+  TPL_ASSERT(plan.GetChildrenSize() == 1, "Projections expected to have one child");
+  // Register this operator in the pipeline.
+  pipeline->RegisterStep(this, Pipeline::Parallelism::Parallel);
+  // Prepare children for codegen.
+  compilation_context->Prepare(*plan.GetChild(0), pipeline);
+}
 
 void ProjectionTranslator::PerformPipelineWork(WorkContext *work_context,
                                                FunctionBuilder *function) const {
