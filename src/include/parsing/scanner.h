655ccf4302e72a42f44863d8f04ce49476152b1c
Prashanth Menon <pmenon@cs.cmu.edu>
2020-10-26 10:08:20 -0700

    Cleanup

--- src/include/parsing/scanner.h
+++ src/include/parsing/scanner.h
@@ -10,7 +10,7 @@
 namespace tpl::parsing {
 
 /**
- * Token scanner
+ * Token scanner for TPL source.
  */
 class Scanner {
   static constexpr int32_t kEndOfInput = -1;
@@ -19,53 +19,56 @@ class Scanner {
 
  public:
   /**
-   * Construct a scanner over the given input string
-   * @param source Input TPL source
-   * @param source_len Length (in bytes) of input source
+   * Construct a scanner over the given input string.
+   * @param source Input TPL source.
+   * @param source_len Length (in bytes) of input source.
    */
   Scanner(const char *source, uint64_t source_len);
 
   /**
-   * Construct a scanner over the given input string @em source
-   * @param source The input TPL source
+   * Construct a scanner over the given input string @em source.
+   * @param source The input TPL source.
    */
   explicit Scanner(const std::string &source);
 
   /**
-   * This class cannot be copied or moved
+   * This class cannot be copied or moved.
    */
   DISALLOW_COPY_AND_MOVE(Scanner);
 
   /**
-   * Move the scanner ahead by one token, returning the token its read
+   * Move the scanner ahead by one token.
+   * @return The token that is read.
    */
   Token::Type Next();
 
   /**
-   * Peek at the next token without moving ahead
+   * Look ahead one token in the token stream without advancing the scanner.
+   * @return The next token in the stream.
    */
-  Token::Type peek() const { return next_.type; }
+  Token::Type Peek() const { return next_.type; }
 
   /**
-   * Return the token the scanner is currently pointing to
+   * @return The token the scanner is currently positioned at.
    */
-  Token::Type current_token() const { return curr_.type; }
+  Token::Type CurrentToken() const { return curr_.type; }
 
   /**
-   * If the current token is a literal, return the literal value
+   * @return The string value of the token at the current scan position. Valid only if the current
+   *         token is a literal.
    */
-  const std::string &current_literal() const { return curr_.literal; }
+  const std::string &CurrentLiteral() const { return curr_.literal; }
 
   /**
-   * Return the position (line and column) of the scanner
+   * @return The position (line and column) of the scanner.
    */
-  const SourcePosition &current_position() const { return curr_.pos; }
+  const SourcePosition &CurrentPosition() const { return curr_.pos; }
 
  private:
-  // Scan the next token
+  // Scan the next token.
   void Scan();
 
-  // Advance a single character into the source input stream
+  // Advance a single character into the source input stream.
   void Advance() {
     bool at_end = (offset_ == source_len_);
     if (TPL_UNLIKELY(at_end)) {
@@ -78,7 +81,7 @@ class Scanner {
     c0_pos_.column++;
   }
 
-  // Advance until the given predicate returns false or we reach the end of the input
+  // Advance until the given predicate returns false or we reach the end of the input.
   template <typename P>
   void AdvanceUntil(P &&p) {
     while (c0_ != kEndOfInput && !p(c0_)) {
@@ -86,7 +89,7 @@ class Scanner {
     }
   }
 
-  // Does the current character match the expected? If so, advance the scanner
+  // Does the current character match the expected? If so, advance the scanner.
   bool Matches(int32_t expected) {
     if (c0_ != expected) {
       return false;
@@ -97,25 +100,25 @@ class Scanner {
     return true;
   }
 
-  // Skip whitespace from the current token to the next valid token
+  // Skip whitespace from the current token to the next valid token.
   void SkipWhiteSpace();
 
-  // Skip a line comment
+  // Skip a line comment.
   void SkipLineComment();
 
-  // Skip a block comment
+  // Skip a block comment.
   void SkipBlockComment();
 
-  // Scan an identifier token
+  // Scan an identifier token.
   Token::Type ScanIdentifierOrKeyword();
 
-  // Check if the given input is a keyword or an identifier
+  // Check if the given input is a keyword or an identifier.
   Token::Type CheckIdentifierOrKeyword(const char *input, uint32_t input_len);
 
-  // Scan a number literal
+  // Scan a number literal.
   Token::Type ScanNumber();
 
-  // Scan a string literal
+  // Scan a string literal.
   Token::Type ScanString();
 
   /**
@@ -139,7 +142,7 @@ class Scanner {
     return (c >= lower && c <= upper);
   }
 
-  // Is the provided character an alphabetic character
+  // Is the provided character an alphabetic character.
   static bool IsAlpha(int32_t c) { return IsInRange(c, 'a', 'z') || IsInRange(c, 'A', 'Z'); }
 
   // Is the current character a digit?
@@ -149,18 +152,18 @@ class Scanner {
   static bool IsIdentifierChar(int32_t c) { return IsAlpha(c) || IsDigit(c) || c == '_'; }
 
  private:
-  // The source input and its length
+  // The source input and its length.
   const char *source_;
   uint64_t source_len_;
 
-  // The offset/position in the source where the next character is read from
+  // The offset/position in the source where the next character is read from.
   uint64_t offset_;
 
-  // The lookahead character and its position in the source
+  // The lookahead character and its position in the source.
   int32_t c0_;
   SourcePosition c0_pos_;
 
-  // Information about the current and next token in the input stream
+  // Information about the current and next token in the input stream.
   TokenDesc curr_;
   TokenDesc next_;
 };
