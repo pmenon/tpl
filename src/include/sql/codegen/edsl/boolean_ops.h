27e2b52a0106c897179d57cc902a41ced0a64952
Prashanth Menon <pmenon@cs.cmu.edu>
2021-01-13 09:28:13 -0800

    Opts

--- src/include/sql/codegen/edsl/boolean_ops.h
+++ src/include/sql/codegen/edsl/boolean_ops.h
@@ -11,7 +11,18 @@ namespace tpl::sql::codegen::edsl {
  * @return The result of the conjunction.
  */
 inline Value<bool> operator&&(const Value<bool> &lhs, const Value<bool> &rhs) {
-  return Value<bool>(lhs.GetCodeGen(), lhs.GetCodeGen()->LogicalAnd(lhs.GetRaw(), rhs.GetRaw()));
+  CodeGen *codegen = lhs.GetCodeGen();
+  if (auto [left_lit, right_lit] = std::make_tuple(lhs.GetRaw()->SafeAs<ast::LiteralExpression>(),
+                                                   rhs.GetRaw()->SafeAs<ast::LiteralExpression>());
+      left_lit != nullptr && right_lit != nullptr) {
+    return Value<bool>(codegen, codegen->Literal(left_lit->BoolVal() && right_lit->BoolVal()));
+  } else if (left_lit != nullptr) {
+    return Value<bool>(codegen, !left_lit->BoolVal() ? codegen->Literal(false) : rhs.GetRaw());
+  } else if (right_lit != nullptr) {
+    return Value<bool>(codegen, !right_lit->BoolVal() ? codegen->Literal(false) : lhs.GetRaw());
+  } else {
+    return Value<bool>(codegen, codegen->LogicalAnd(lhs.GetRaw(), rhs.GetRaw()));
+  }
 }
 
 /**
@@ -21,7 +32,18 @@ inline Value<bool> operator&&(const Value<bool> &lhs, const Value<bool> &rhs) {
  * @return The result of the conjunction.
  */
 inline Value<bool> operator||(const Value<bool> &lhs, const Value<bool> &rhs) {
-  return Value<bool>(lhs.GetCodeGen(), lhs.GetCodeGen()->LogicalOr(lhs.GetRaw(), rhs.GetRaw()));
+  CodeGen *codegen = lhs.GetCodeGen();
+  if (auto [left_lit, right_lit] = std::make_tuple(lhs.GetRaw()->SafeAs<ast::LiteralExpression>(),
+                                                   rhs.GetRaw()->SafeAs<ast::LiteralExpression>());
+      left_lit != nullptr && right_lit != nullptr) {
+    return Value<bool>(codegen, codegen->Literal(left_lit->BoolVal() || right_lit->BoolVal()));
+  } else if (left_lit != nullptr) {
+    return Value<bool>(codegen, left_lit->BoolVal() ? codegen->Literal(true) : rhs.GetRaw());
+  } else if (right_lit != nullptr) {
+    return Value<bool>(codegen, right_lit->BoolVal() ? codegen->Literal(true) : lhs.GetRaw());
+  } else {
+    return Value<bool>(codegen, codegen->LogicalOr(lhs.GetRaw(), rhs.GetRaw()));
+  }
 }
 
 /**
@@ -31,7 +53,12 @@ inline Value<bool> operator||(const Value<bool> &lhs, const Value<bool> &rhs) {
  * @return The result of the negation.
  */
 inline Value<bool> operator!(const Value<bool> &input) {
-  return Value<bool>(input.GetCodeGen(), input.GetCodeGen()->LogicalNot(input.GetRaw()));
+  CodeGen *codegen = input.GetCodeGen();
+  if (auto expr = input.GetRaw()->SafeAs<ast::LiteralExpression>()) {
+    return Value<bool>(codegen, codegen->Literal(!expr->BoolVal()));
+  } else {
+    return Value<bool>(codegen, codegen->LogicalNot(input.GetRaw()));
+  }
 }
 
 }  // namespace tpl::sql::codegen::edsl
