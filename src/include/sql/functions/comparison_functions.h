13485edcc22bf1405a97ee362dbd0d7688f1125b
Prashanth Menon <pmenon@cs.cmu.edu>
2021-01-20 23:34:12 -0800

    Lint.

--- src/include/sql/functions/comparison_functions.h
+++ src/include/sql/functions/comparison_functions.h
@@ -14,60 +14,143 @@ namespace tpl::sql {
  */
 class ComparisonFunctions : public AllStatic {
  public:
+  /**
+   * Compute the logical negation of the input boolean value, i.e., !input.
+   * @param[out] result Where the result of the operation is stored.
+   * @param input The input.
+   */
   static void NotBoolVal(BoolVal *result, const BoolVal &input) {
     UnaryFunction::EvalFast(result, input, tpl::sql::Not{});
   }
 
+  /**
+   * Perform an equality comparison, i.e., result = a == b.
+   * @tparam T The SQL value types of the input.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   template <SQLValueType T>
   static void Equal(BoolVal *result, const T &a, const T &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::Equal<decltype(T::val)>{});
   }
 
+  /**
+   * Perform an equality comparison on two string inputs, i.e., result = a == b.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   static void Equal(BoolVal *result, const StringVal &a, const StringVal &b) {
     BinaryFunction::EvalHideNull(result, a, b, tpl::sql::Equal<VarlenEntry>{});
   }
 
+  /**
+   * Perform an equality comparison, i.e., result = a >= b.
+   * @tparam T The SQL value types of the input.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   template <SQLValueType T>
   static void GreaterEqual(BoolVal *result, const T &a, const T &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::GreaterThanEqual<decltype(T::val)>{});
   }
 
+  /**
+   * Perform an equality comparison on two string inputs, i.e., result = a >= b.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   static void GreaterEqual(BoolVal *result, const StringVal &a, const StringVal &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::GreaterThanEqual<VarlenEntry>{});
   }
 
+  /**
+   * Perform an equality comparison, i.e., result = a > b.
+   * @tparam T The SQL value types of the input.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   template <SQLValueType T>
   static void GreaterThan(BoolVal *result, const T &a, const T &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::GreaterThan<decltype(T::val)>{});
   }
 
+  /**
+   * Perform an equality comparison on two string inputs, i.e., result = a > b.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   static void GreaterThan(BoolVal *result, const StringVal &a, const StringVal &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::GreaterThan<VarlenEntry>{});
   }
 
+  /**
+   * Perform an equality comparison, i.e., result = a <= b.
+   * @tparam T The SQL value types of the input.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   template <SQLValueType T>
   static void LessEqual(BoolVal *result, const T &a, const T &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::LessThanEqual<decltype(T::val)>{});
   }
 
+  /**
+   * Perform an equality comparison on two string inputs, i.e., result = a <= b.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   static void LessEqual(BoolVal *result, const StringVal &a, const StringVal &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::LessThanEqual<VarlenEntry>{});
   }
 
+  /**
+   * Perform an equality comparison, i.e., result = a < b.
+   * @tparam T The SQL value types of the input.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   template <SQLValueType T>
   static void LessThan(BoolVal *result, const T &a, const T &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::LessThan<decltype(T::val)>{});
   }
 
+  /**
+   * Perform an equality comparison on two string inputs, i.e., result = a < b.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   static void LessThan(BoolVal *result, const StringVal &a, const StringVal &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::LessThan<VarlenEntry>{});
   }
 
+  /**
+   * Perform an equality comparison, i.e., result = a != b.
+   * @tparam T The SQL value types of the input.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   template <SQLValueType T>
   static void NotEqual(BoolVal *result, const T &a, const T &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::NotEqual<decltype(T::val)>{});
   }
 
+  /**
+   * Perform an equality comparison on two string inputs, i.e., result = a != b.
+   * @param[out] result Where the result of the comparison is stored.
+   * @param a The left input to the comparison.
+   * @param b The right input to the comparison.
+   */
   static void NotEqual(BoolVal *result, const StringVal &a, const StringVal &b) {
     BinaryFunction::EvalFast(result, a, b, tpl::sql::NotEqual<VarlenEntry>{});
   }
